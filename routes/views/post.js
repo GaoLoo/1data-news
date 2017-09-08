7c633912db48666375884979ae914b4e267b2ed9
gaolu <www.495501790@qq.com>
2017-09-08 16:10:30 +0800

    修改语言

---
+++ routes/views/post.js
@@ -0,0 +1,71 @@
+var keystone = require('keystone');
+var async = require('async');
+exports = module.exports = function (req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// Set locals
+	locals.section = 'home';
+	locals.filters = {
+		post: req.params.post,
+		category: req.params.category
+	};
+	locals.data = {
+		posts: [],
+		categories: [],
+	};
+
+	// Load the current post
+	view.on('init', function (next) {
+
+		var q = keystone.list('Post').model.findOne({
+			state: 'published',
+			slug: locals.filters.post,
+		}).populate('author categories');
+
+		q.exec(function (err, result) {
+			locals.data.post = result;
+			next(err);
+		});
+
+	});
+	// Load all categories
+	view.on('init', function (next) {
+
+		keystone.list('PostCategory').model.find().sort('name').exec(function (err, results) {
+
+			if (err || !results.length) {
+				return next(err);
+			}
+
+			locals.data.categories = results;
+
+			// Load the counts for each category
+			async.each(locals.data.categories, function (category, next) {
+				keystone.list('Post').model.count().where('categories').in([category.id]).exec(function (err, count) {
+					category.postCount = count;
+					next(err);
+				});
+
+			}, function (err) {
+				next(err);
+			});
+		});
+	});
+
+	// Load other posts
+	view.on('init', function (next) {
+
+		var q = keystone.list('Post').model.find().where('state', 'published').sort('-publishedDate').populate('author').limit('4');
+
+		q.exec(function (err, results) {
+			locals.data.posts = results;
+			next(err);
+		});
+
+	});
+
+	// Render the view
+	view.render('post');
+};
